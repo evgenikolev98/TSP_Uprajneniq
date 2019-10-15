37dc9ae075cd011ec7bceeab184a78e67e491d5d
Evgeni Kolev <evgeni.kolev98@gmail.com>
2019-10-15 18:32:48 +0300

    lab3

---
+++ Lab_upr3/Zadacha 3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year = Int32.Parse(textBox1.Text);
+            int month = Int32.Parse(textBox2.Text);
+            bool t = DateTime.IsLeapYear(year);
+            try 
+            {
+                validate(year, month);
+                if (t)
+                {
+                    MessageBox.Show(" Годината е високосна ", "");
+                }
+                else
+                {
+                    MessageBox.Show(" Годината не е високосна ", "");
+                }
+            }
+            catch { }
+        }
+
+        private void validate(int y, int m)
+        {
+            if (m < 1 || m > 12)
+            {
+                MessageBox.Show("Въведете месец от 1-ви до 12-ти ", "");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else if (y < 1900)
+            {
+                MessageBox.Show(" Въведете година след 1900 ","");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+    }
+}
---
+++ Lab_upr3/Zadacha4/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadacha4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year, day, month;
+             month = Int32.Parse(textBox1.Text);
+             day = Int32.Parse(textBox2.Text);
+             year = Int32.Parse(textBox3.Text);
+            Calc(year, month);
+       
+        }
+
+        int  Calc(int y, int m)
+        {
+            int result_year, result_month, year_today, month_today;
+            year_today = DateTime.Today.Year;
+            month_today = DateTime.Today.Month;
+            result_year = year_today - y;
+            result_month = month_today - m;
+
+            if (month_today < m)
+            {
+                result_year -= 1;
+                result_month += 12;
+            }
+            MessageBox.Show(result_year + " Years and " + result_month + " Months","");
+            return 0;
+        }
+    }
+}
